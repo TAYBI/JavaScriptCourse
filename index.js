0ab9adadf09722a3f8d1b5f0a2f8ac88e281c5f5
Bilal <taybib4@gmail.com>
2023-02-24 23:45:11 +0100

    init Javascript course

---
+++ index.js
@@ -0,0 +1,9 @@
+let todo1 = 'make one thing above'
+let todo2 = 'yake a walk'
+let todo3 = 'make fire'
+let todo4 = 'smoke shit'
+
+let element = document.createElement('div')
+console.log(element);
+element.innerText = todo1
+document.body.appendChild(element)
