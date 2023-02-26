cb92b403a6c2bebcb280edaf6d92ba237d158f98
Bilal <taybib4@gmail.com>
2023-02-26 23:07:27 +0100

    EX7

--- index.js
+++ index.js
@@ -14,3 +14,22 @@
 // for (let i = 0; i < todos.length; i++) {
 //     todos[i].innerHTML = todosText[todos.length - i - 1];
 // }
+//--------------------------------------------------------
+// EX7 ////////////////////////
+//1-
+// words = ['word1', 'word2', 'word3', 'word4']
+// function toUpper(arg) {
+//     arg.forEach(element => {
+//         console.log(element.toUpperCase());
+//     });
+// }
+// toUpper(words)
+//2-
+// words = ['word1', 'word2', 'word3']
+// function arrayDouble(array) {
+//     array.forEach(element => {
+//         array.push(element)
+//     });
+// }
+// arrayDouble(words)
+// console.log(words);
