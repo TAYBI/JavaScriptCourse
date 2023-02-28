1e1d26b18087856f47e659e84eaa0a5ec27ad597
Bilal <taybib4@gmail.com>
2023-02-28 16:23:10 +0100

    add delete button

--- index.js
+++ index.js
@@ -44,20 +44,29 @@
 
 const todos = [{
     title: 'todo title1',
-    dueDate: '2023-04-14'
+    dueDate: '2023-04-14',
+    id: 'id45678'
 }, {
     title: 'todo title2',
-    dueDate: '2023-02-31'
+    dueDate: '2023-02-31',
+    id: 'id456788'
 }]
 
 function render() {
     // getting the to dos container
-    const todosContainer = document.getElementById('todo-list')
-    todosContainer.innerHTML = ''
+    const todosContainer = document.getElementById('todo-list');
+    todosContainer.innerHTML = '';
     // adding todos to their container
     todos.forEach((element) => {
+        deleteButton = document.createElement('button');
+        deleteButton.innerText = 'delete';
+        deleteButton.id = element.id;
+        deleteButton.style = "margin-left: 10px";
+        deleteButton.onclick = deleteTodo
+
         const child = document.createElement('p');
         child.innerHTML = element.title + " " + element.dueDate;
+        child.appendChild(deleteButton)
         todosContainer.appendChild(child);
     });
 }
@@ -69,8 +78,14 @@ function addTodo() {
     const dueDate = document.getElementById('date').value;
     todo = {
         title: title,
-        dueDate: dueDate
+        dueDate: dueDate,
+        id: new Date().getTime()
     }
     todos.push(todo);
     render();
+}
+
+function deleteTodo(e) {
+    console.log(e.target.id);
+    console.log('delete');
 }
