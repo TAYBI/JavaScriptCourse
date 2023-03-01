ca35e013e6a4f3684997ae6a826c530cb7fef4f0
Bilal <taybib4@gmail.com>
2023-03-01 08:14:30 +0100

    switch to MVC

--- Exercises/main.js
+++ Exercises/main.js
@@ -1,3 +1,48 @@
+
+// Ex 5 /////////////////////
+//1-
+// document.getElementById('btn').addEventListener("click", function () {
+//     document.title = 'New Title'
+// }, false)
+//
+// todos = document.getElementsByTagName('p')
+// todosText = []
+// for (let i = 0; i < todos.length; i++) {
+//     const element = todos[i].innerHTML;
+//     todosText.push(element)
+// }
+//2-
+// for (let i = 0; i < todos.length; i++) {
+//     todos[i].innerHTML = todosText[todos.length - i - 1];
+// }
+//--------------------------------------------------------
+// EX7 ////////////////////////
+//1-
+// words = ['word1', 'word2', 'word3', 'word4']
+// function toUpper(arg) {
+//     arg.forEach(element => {
+//         console.log(element.toUpperCase());
+//     });
+// }
+// toUpper(words)
+//2-
+// words = ['word1', 'word2', 'word3']
+// function arrayDouble(array) {
+//     array.forEach(element => {
+//         array.push(element)
+//     });
+// }
+// arrayDouble(words)
+// console.log(words);
+//--------------------------------------------------------
+// EX8 ////////////////////////
+// button = document.createElement('button')
+// button.innerHTML = "click"
+// button.addEventListener("click", function () {
+//     button.innerText = 'done'
+// }, false)
+// document.body.appendChild(button)
+/////////////////////////////
 // EX 9
 function addToCart(btn) {
     document.getElementById('cart').innerHTML += ' - ' + btn.innerText
--- index.js
+++ index.js
@@ -1,47 +1,4 @@
-// Ex 5 /////////////////////
-//1-
-// document.getElementById('btn').addEventListener("click", function () {
-//     document.title = 'New Title'
-// }, false)
-//
-// todos = document.getElementsByTagName('p')
-// todosText = []
-// for (let i = 0; i < todos.length; i++) {
-//     const element = todos[i].innerHTML;
-//     todosText.push(element)
-// }
-//2-
-// for (let i = 0; i < todos.length; i++) {
-//     todos[i].innerHTML = todosText[todos.length - i - 1];
-// }
-//--------------------------------------------------------
-// EX7 ////////////////////////
-//1-
-// words = ['word1', 'word2', 'word3', 'word4']
-// function toUpper(arg) {
-//     arg.forEach(element => {
-//         console.log(element.toUpperCase());
-//     });
-// }
-// toUpper(words)
-//2-
-// words = ['word1', 'word2', 'word3']
-// function arrayDouble(array) {
-//     array.forEach(element => {
-//         array.push(element)
-//     });
-// }
-// arrayDouble(words)
-// console.log(words);
-//--------------------------------------------------------
-// EX8 ////////////////////////
-// button = document.createElement('button')
-// button.innerHTML = "click"
-// button.addEventListener("click", function () {
-//     button.innerText = 'done'
-// }, false)
-// document.body.appendChild(button)
-
+////////////// Model
 let todos = [{
     title: 'todo title1',
     dueDate: '2023-04-14',
@@ -51,7 +8,27 @@ let todos = [{
     dueDate: '2023-02-31',
     id: 'id456788'
 }]
+// creates todo
+function createTodo(title, dueDate) {
+    todo = {
+        title: title,
+        dueDate: dueDate,
+        id: '' + new Date().getTime()
+    }
+    todos.push(todo);
+}
 
+// deletes totdo
+function removeTodo(idToDelete) {
+    todos = todos.filter(function (todo) {
+        if (todo.id === idToDelete) {
+            return false;
+        } else {
+            return true;
+        }
+    })
+}
+////////////////// View
 function render() {
     // getting the to dos container
     const todosContainer = document.getElementById('todo-list');
@@ -70,18 +47,13 @@ function render() {
         todosContainer.appendChild(child);
     });
 }
-
 render()
 
+/////////// Controler
 function addTodo() {
     const title = document.getElementById('todoTitle');
     const dueDate = document.getElementById('date');
-    todo = {
-        title: title.value,
-        dueDate: dueDate.value,
-        id: '' + new Date().getTime()
-    }
-    todos.push(todo);
+    createTodo(title.value, dueDate.value);
     render();
     title.value = ''
     dueDate.value = ''
@@ -89,16 +61,6 @@ function addTodo() {
 
 function deleteTodo(e) {
     const idToDelete = e.target.id;
-
-    todos = todos.filter(function (todo) {
-        if (todo.id === idToDelete) {
-            return false;
-        } else {
-            return true;
-        }
-    })
-
-    console.log(e.target.id);
-    console.log(todos);
+    removeTodo(idToDelete)
     render()
 }
